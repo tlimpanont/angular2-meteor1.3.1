d7827433eda8faf4ca3674d98b7fd729f991b68b
Theuy Limpanont <theuy.limpanont@gmail.com>
2016-04-11 22:30:17 +0200

    add typings defs and tsconfig based on angular2-meteor tutorial

--- client/app.ts
+++ client/app.ts
@@ -1,10 +1,12 @@
 import {Component} from 'angular2/core';
-import {bootstrap} from 'angular2-meteor-auto-bootstrap';
+import {bootstrap} from 'angular2/platform/browser';
 
 @Component({
   selector: 'app',
   templateUrl: 'client/index.html'
 })
-export class App {}
+export class App {
+  public Meteor:Object = Meteor;
+}
 
 bootstrap(App);
