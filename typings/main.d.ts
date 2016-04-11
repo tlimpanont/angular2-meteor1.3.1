1c759acc0c2f678ec233263d8d957aac88fd9a5e
Theuy Limpanont <theuy.limpanont@gmail.com>
2016-04-11 22:37:38 +0200

    add angular2-meteor typings

--- typings/main.d.ts
+++ typings/main.d.ts
@@ -14,3 +14,4 @@
 /// <reference path="main/ambient/es6-shim/index.d.ts" />
 /// <reference path="main/ambient/meteor/index.d.ts" />
 /// <reference path="main/definitions/es6-promise/index.d.ts" />
+/// <reference path="main/angular2-meteor/index.d.ts" />
